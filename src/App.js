24c94893cad0a5f9c2ae35e8ee841943d9bf627c
topazmania <tockdockie@gmail.com>
2023-12-05 11:25:18 -0300

    aaaaa

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import sub from './func/func';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          1-1={sub(1, 1)}
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
