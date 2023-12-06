e865f5e7dad5591dbc2bd6ea22387dcd9d1c7a98
topazmania <tockdockie@gmail.com>
2023-12-06 09:13:35 -0300

    kk

---
+++ exe4/src/App.js
@@ -0,0 +1,8 @@
+import Shsh from './aaaaaaaaa/aaaa.js';
+import './App.css';
+
+function App() {
+  return <Shsh name = "luizinho 100%" > </Shsh>
+}
+
+export default App;
---
+++ exe4/src/aaaaaaaaa/aaaa.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function Shsh (props) {
+    return (<h1> Bem vindo, {props.name} </h1>)
+}
+export default Shsh;
